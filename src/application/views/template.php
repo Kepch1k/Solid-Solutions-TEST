668ac6445f6ddc638d11eed49c6b230483b5560a
Kepch1k <kephik2000@gmail.com>
2020-09-28 23:19:51 +0300

    update template (set new title for modal window)

--- src/application/views/template.php
+++ src/application/views/template.php
@@ -93,7 +93,7 @@
 			<div class="modal-dialog modal-dialog-centered" role="document">
 				<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLongTitle">Delete node</h5>
+					<h5 class="modal-title" id="exampleModalLongTitle">Delete root node</h5>
 					<button id="ExitDeleteModalButton" type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 					</button>
