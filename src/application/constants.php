d26f1670997e4542e8ecffa7e76d4c1a6b1a4518
Kepch1k <kephik2000@gmail.com>
2020-09-28 19:23:31 +0300

    add file with database constants

---
+++ src/application/constants.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_DRIVER', 'mysql');
+define('DB_URL', '127.0.0.1');
+define('DB_NAME', 'solid_solutions_test');
+define('DB_LOGIN', 'root');
+define('DB_PASSWORD', '');
+define('DB_DSN', DB_DRIVER . ':dbname=' . DB_NAME . ';host=' . DB_URL);
